4acc6b84fd17d752b8daad2a1a0d003287888b53
Steven Hollingsworth <shollingsworth@barracuda.com>
2021-11-19 16:43:11 -0800

    test

--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
+# set -euo pipefail
+# IFS=$'\n\t'
 
-sudo rm -rfv ./pg-data/
-docker build -t alerta-test -f ./alerta.Dockerfile .
+# sudo rm -rfv ./pg-data/
+# docker build -t alerta-test -f ./alerta.Dockerfile .
+export TEST="foo"
+if [[ "${TEST}-" == "-" ]]; then
+    echo "TEST not set"
+    exit 1
+else
+    echo "OK"
+fi
